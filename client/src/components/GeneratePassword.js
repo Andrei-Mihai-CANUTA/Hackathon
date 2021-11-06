8003dae04987407c99d39fa8702544d149dd8b6b
Tudor73 <tudorminciunescu@gmail.com>
2021-11-06 20:27:40 +0200

    post requests

--- client/src/components/GeneratePassword.js
+++ client/src/components/GeneratePassword.js
@@ -1,14 +1,71 @@
-import react from "react";
+import react, { useState } from "react";
+import crypto from "crypto";
 
 export default function GeneratePassword() {
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+  const [passwordLength, setPasswordLength] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const passwordGenerated = crypto
+      .randomBytes(parseInt(passwordLength))
+      .toString("hex");
+
+    const data = {
+      name: name,
+      url: url,
+      password: passwordGenerated,
+    };
+    fetch("http://localhost:4000/addAccount", {
+      method: "POST",
+      // mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
   return (
     <div>
-      <label for="url">Add site name</label>
-      <input type="text" name="name" />
-      <label for="url">Add site url</label>
-      <input type="text" name="url" />
-      <label>Password length </label>
-      <input type="text" />
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <label htmlFor="url">Add site name</label>
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <label htmlFor="url">Add site url</label>
+        <input
+          type="text"
+          name="url"
+          onChange={(e) => {
+            setUrl(e.target.value);
+          }}
+        />
+        <label>Password length </label>
+        <input
+          type="text"
+          value={passwordLength}
+          onChange={(e) => {
+            setPasswordLength(e.target.value);
+          }}
+        />
+        <button type="submit">Add Account</button>
+      </form>
     </div>
   );
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,16 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./form.css";
 function Register() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => {
+    const data = {
+      name: name,
+      password: password,
+    };
+
+    fetch("http://localhost:4000/addUser", {
+      method: "POST",
+      // mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
   return (
-    <div class = "container">
-    <ul class = "listamain">
-        <li><h1>Register:</h1></li>
-        <li><input type = "text" name = "Username" placeholder = "Username:"/></li>
-        <li><input type = "password" name = "Password" placeholder = "Password"/></li>
-        <li><input type = "password" name = "Password" placeholder = "Repeat password"/></li>
-        <li><input type = "button" name = "Submit" value = "Submit"/></li>
-    </ul>
-</div>
+    <div class="container">
+      <ul class="listamain">
+        <li>
+          <h1>Register:</h1>
+        </li>
+        <li>
+          <input
+            type="text"
+            name="Username"
+            placeholder="Username:"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </li>
+        <li>
+          <input
+            type="password"
+            name="Password"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </li>
+        <li>
+          <input
+            type="password"
+            name="Password"
+            placeholder="Repeat password"
+          />
+        </li>
+        <li>
+          <input
+            type="button"
+            name="Submit"
+            value="Submit"
+            onClick={() => handleSubmit()}
+          />
+        </li>
+      </ul>
+    </div>
   );
 }
 export default Register;
