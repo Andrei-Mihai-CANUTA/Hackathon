f2761ef75c8e7937a496400d154e6132a4104e4c
Tudor73 <tudorminciunescu@gmail.com>
2021-11-06 15:08:05 +0200

    react router

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+function Dashbaord() {
+  return <div>Dashbord</div>;
+}
+
+export default Dashbaord;
---
+++ client/src/components/Form.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+function Form() {
+  return (
+    <div>
+      <form>
+        <label for="email">Adresa de mail</label>
+        <input type="text" name="email" Adresa de mail />
+        <label for="password">Parola</label>.
+        <input type="password" name="password" Parola />
+      </form>
+    </div>
+  );
+}
+export default Form;
