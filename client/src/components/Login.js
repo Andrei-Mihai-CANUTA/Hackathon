bd8122e694b924cdcaec136fd57931e828821c1a
Khaezar67 <cezarbunea@gmail.com>
2021-11-06 18:41:42 +0200

    Styled website

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,11 +4,11 @@ function Login() {
   return (
     <div class = "container">
     <ul class = "listamain">
-        <li><h1>Login section:</h1></li>
+        <li><h1>Login:</h1></li>
         <li><input type = "text" name = "Username" placeholder = "Username"/></li>
         <li><input type = "password" name = "Password" placeholder = "********"/></li>
         <li><input type = "button" name = "Submit" value = "Submit"/></li>
-        <li><input type = "button" name = "No account?" value = "No acccount?"/></li>
+        <li><a href = "http://localhost:3000/register">Sign up!</a></li>
     </ul>
 </div>
   );
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -4,9 +4,10 @@ function Register() {
   return (
     <div class = "container">
     <ul class = "listamain">
-        <li><h1>Login section:</h1></li>
+        <li><h1>Register:</h1></li>
         <li><input type = "text" name = "Username" placeholder = "Username"/></li>
-        <li><input type = "password" name = "Password" placeholder = "********"/></li>
+        <li><input type = "password" name = "Password" placeholder = "Password"/></li>
+        <li><input type = "password" name = "Password" placeholder = "Repeat password"/></li>
         <li><input type = "button" name = "Submit" value = "Submit"/></li>
     </ul>
 </div>
