cafdb100e1f9a9dbefc4d85ccadc6382857d5668
Tudor73 <tudorminciunescu@gmail.com>
2021-11-06 19:18:40 +0200

    remove posgress

--- server/index.js
+++ server/index.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 4000;
-const database = require("./database.js")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const query = `
-INSERT INTO users (username, password)
-VALUES ('&1', $2)`;
-
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -22,22 +16,16 @@ app.post("/login", (req, res) => {
   res.send("User logged in ");
 });
 
-app.post("/register", (req, res) => {
-
-  database.query(query, [req.body.username], [req.body.password]);
+app.post("/register", async (req, res) => {
   console.log(req.body);
   console.log(req.body.username);
   console.log(req.body.password);
-  console.log(req.body.repeat_password);
-  
 
-  if(req.body.password == req.body.repeat_password){
+  if (req.body.password == req.body.repeat_password) {
     res.send("User registered! ");
-}
-  else{
+  } else {
     res.send("No match between passwords! ");
   }
-
 });
 
 app.listen(port, () => {
