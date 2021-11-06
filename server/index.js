a7f27701a8bc52c6de71020bc3f92b97612e7db5
Tudor73 <tudorminciunescu@gmail.com>
2021-11-06 15:21:27 +0200

    express setup

--- server/index.js
+++ server/index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/login", (req, res) => {});
+app.post("/login", (req, res) => {
+  // get user name and password
+  console.log(req.body);
+  console.log(req.body.name);
+  console.log(req.body.password);
+  res.send("User logged in ");
+});
 
 app.get("/register", (req, res) => {
   // send data to database
