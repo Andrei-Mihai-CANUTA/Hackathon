fa946d6d1fd4b6f60ebab14a23e7739986bdaadf
Tudor73 <tudorminciunescu@gmail.com>
2021-11-06 23:38:04 +0200

    loggin req

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./form.css";
 function Login() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    const data = {
+      username: name,
+      password: password,
+    };
+    fetch("http://localhost:4000/login", {
+      method: "POST",
+      // mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
   return (
-    <div class = "container">
-    <ul class = "listamain">
-        <li><h1>Login:</h1></li>
-        <li><input type = "text" name = "Username" placeholder = "Username"/></li>
-        <li><input type = "password" name = "Password" placeholder = "*********"/></li>
-        <li><input type = "button" name = "Submit" value = "Submit"/></li>
-        <li><a href = "http://localhost:3000/register">Sign up!</a></li>
-    </ul>
-</div>
+    <div class="container">
+      <ul class="listamain">
+        <li>
+          <h1>Login:</h1>
+        </li>
+        <li>
+          <input
+            value={name}
+            type="text"
+            name="Username"
+            placeholder="Username"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </li>
+        <li>
+          <input
+            value={password}
+            type="password"
+            name="Password"
+            placeholder="*********"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </li>
+        <li>
+          <input
+            type="button"
+            name="Submit"
+            value="Submit"
+            onClick={() => handleLogin()}
+          />
+        </li>
+        <li>
+          <a href="http://localhost:3000/register">Sign up!</a>
+        </li>
+      </ul>
+    </div>
   );
 }
 export default Login;
