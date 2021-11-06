7b703af356c7b127778d4cc7ecfb428128d96f8e
Khaezar67 <cezarbunea@gmail.com>
2021-11-06 22:38:46 +0200

    Styled Generate Pass

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,29 +1,33 @@
 import React, { useState } from "react";
 import "./form.css";
+import { useNavigate } from "react-router-dom";
 function Register() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
   const handleSubmit = () => {
     const data = {
       name: name,
       password: password,
     };
 
-    fetch("http://localhost:4000/addUser", {
-      method: "POST",
-      // mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+
+    // fetch("http://localhost:4000/addUser", {
+    //   method: "POST",
+    //   // mode: "cors",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log("Success:", data);
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error:", error);
+    //   });
+      navigate("/dashboard")
   };
   return (
     <div class="container">
