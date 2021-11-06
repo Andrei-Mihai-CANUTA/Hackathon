504b9f354e369cdbea96bd4fe3940f59ff48ed24
Khaezar67 <cezarbunea@gmail.com>
2021-11-06 18:52:59 +0200

    random

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,7 +6,7 @@ function Login() {
     <ul class = "listamain">
         <li><h1>Login:</h1></li>
         <li><input type = "text" name = "Username" placeholder = "Username"/></li>
-        <li><input type = "password" name = "Password" placeholder = "********"/></li>
+        <li><input type = "password" name = "Password" placeholder = "*********"/></li>
         <li><input type = "button" name = "Submit" value = "Submit"/></li>
         <li><a href = "http://localhost:3000/register">Sign up!</a></li>
     </ul>
